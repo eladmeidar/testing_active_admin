559c0d87ff8d30f889ffda15d51e2cb95cf36035
Elad Meidar <elad@shinobidevs.com>
2014-09-24 20:12:34 +0300

    [FIX]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  #devise_for :manager_users, ActiveAdmin::Devise.config
+  devise_for :manager_users, ActiveAdmin::Devise.config
   devise_for :admins, ActiveAdmin::Devise.config
 
   ActiveAdmin.routes(self)
